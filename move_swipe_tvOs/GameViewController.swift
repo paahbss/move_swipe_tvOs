a3979bd8e13c3077a195ffd83055334aaf95c119
Paloma Bispo <palomabispo@alu.ufc.br>
2019-04-06 23:07:17 -0300

    adding table

--- move_swipe_tvOs/GameViewController.swift
+++ move_swipe_tvOs/GameViewController.swift
@@ -16,6 +16,7 @@ class GameViewController: UIViewController {
     var scnScene: SCNScene!
     var cameraNode: SCNNode!
     var tube: SCNTube!
+    var table: SCNPlane!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,25 +24,10 @@ class GameViewController: UIViewController {
         setupScene()
         setupCamera()
         setupTube()
-        
-//        // create and add a light to the scene
-//        let lightNode = SCNNode()
-//        lightNode.light = SCNLight()
-//        lightNode.light!.type = .omni
-//        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-//        scene.rootNode.addChildNode(lightNode)
-//
-//        // create and add an ambient light to the scene
-//        let ambientLightNode = SCNNode()
-//        ambientLightNode.light = SCNLight()
-//        ambientLightNode.light!.type = .ambient
-//        ambientLightNode.light!.color = UIColor.darkGray
-//        scene.rootNode.addChildNode(ambientLightNode)
-        
+        setupTable()
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         self.scnView.addGestureRecognizer(tapGesture)
     }
-    
     private func setupView(){
         if  let scnView = self.view as? SCNView {
             self.scnView = scnView
@@ -51,11 +37,11 @@ class GameViewController: UIViewController {
             self.scnView.backgroundColor = UIColor.black
         }
     }
-    private func setupScene(){
+    private func setupScene() {
         self.scnScene = SCNScene()
         self.scnView.scene = self.scnScene
     }
-    private func setupCamera(){
+    private func setupCamera() {
         self.cameraNode = SCNNode()
         self.cameraNode.camera = SCNCamera()
         self.cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
@@ -63,16 +49,23 @@ class GameViewController: UIViewController {
         self.cameraNode.eulerAngles = SCNVector3Make(0, 0, 0);
         self.scnScene.rootNode.addChildNode(self.cameraNode)
     }
-    private func setupTube(){
+    private func setupTube() {
         self.tube = SCNTube(innerRadius: 0.25, outerRadius: 0.5, height: 5.0)
         let node = SCNNode(geometry: self.tube)
         node.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
         node.position = SCNVector3(x: 0.0, y: -2.5, z: 12)
         node.eulerAngles = SCNVector3Make(-80, 0, 0);
         self.scnScene.rootNode.addChildNode(node)
-
     }
-    
+    private func setupTable() {
+        let widthScreen = UIScreen.main.bounds.width
+        self.table = SCNPlane(width: 20, height: 20)
+        let node = SCNNode(geometry: self.table)
+        node.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+        node.position = SCNVector3(x: 0.0, y: 0.0, z: 12)
+        node.eulerAngles = SCNVector3Make(0, 0, 0);
+        self.scnScene.rootNode.addChildNode(node)
+    }
     @objc
     func handleTap(_ gestureRecognize: UIGestureRecognizer) {
     
